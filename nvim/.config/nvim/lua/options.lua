f84eee40d6bced429a02a19d2f73442d148e119c
Huy Nguyen <thenameiswiiwin@Huys-MacBook-Pro.local>
2021-11-28 19:47:33 -0600

    lua nvim options

---
+++ nvim/.config/nvim/lua/options.lua
@@ -0,0 +1,78 @@
+-- Options
+local Type = {GLOBAL_OPTION = "o"}
+local add_options = function(option_type, options)
+  if type(options) ~= "table" then
+    error 'options should be a type of "table"'
+    return
+  end
+  local vim_option = vim[option_type]
+  for key, value in pairs(options) do
+    vim_option[key] = value
+  end
+end
+local Option = {}
+Option.g = function(options)
+  add_options(Type.GLOBAL_OPTION, options)
+end
+
+Option.g {
+    encoding = 'utf-8',
+    startofline = true,
+	expandtab = true,
+    errorbells = true,
+    tabstop = 4,
+    softtabstop = 4,
+    shiftwidth = 4,
+    hidden = true,
+    signcolumn = 'yes',
+    modelines = 0,
+    relativenumber = true,
+    number = true,
+    numberwidth = 3,
+    termguicolors = true,
+    undofile = true,
+    spell = true,
+    title = true,
+    ignorecase = true,
+    smartcase = true,
+    wildmode = 'longest:full,full',
+    wrap = false,
+    list = true,
+    listchars = 'tab:▸·,trail:·',
+    mouse = 'a',
+    scrolloff = 8,
+    sidescrolloff = 8,
+    joinspaces = false,
+    splitright = true,
+    clipboard = 'unnamedplus',
+    confirm = true,
+    backup = true,
+    writebackup = false,
+    backupdir = '$HOME/.local/share/nvim/backup//',
+    undodir = '$HOME/.local/share/nvim/undo//',
+    directory = '$HOME/.local/share/nvim/swap//',
+    updatetime = 300,
+    redrawtime = 10000,
+    swapfile = false,
+    incsearch = true,
+    foldenable = false,
+    foldlevel = 4,
+    foldmethod = 'syntax',
+    synmaxcol = 300,
+    laststatus = 2,
+    showmatch  = true,
+    showcmd = true,
+    autoindent = true,
+    smartindent = true,
+    formatoptions = 'qnj1',
+}
+vim.g.clipboard = {
+  name = "macOS-clipboard",
+  copy = {["+"] = "pbcopy", ["*"] = "pbcopy"},
+  paste = {["+"] = "pbpaste", ["*"] = "pbpaste"},
+  cache_enabled = false
+}
+
+vim.g.mapleader = " "
+
+return Option
